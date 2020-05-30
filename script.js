6945dd22a28780e21ef414f3ff993c055190a3d3
Vedvardhan <vedvardhan.g@gmail.com>
2020-05-30 18:01:36 +0530

    Added cards for global and india stats

--- script.js
+++ script.js
@@ -1,31 +1,54 @@
 // getting the elements
 let enteredDate = document.getElementById('dateBox');
 let months = document.getElementById('months');
-function submit(){
- // adds prefix 0 if the enteredDate.value is a single digit    
+let enteredValue;
+let month;
+const setDate = () => {
     if(enteredDate.value.length === 1){
         enteredDate.value = '0' + enteredDate.value;
     }
- // concatenation of the date and month    
-    let month = months.options[months.selectedIndex].value;
-    let enteredValue = `${enteredDate.value} ${month}` 
+    // concatenation of the date and month    
+    month = months.options[months.selectedIndex].value;
+    enteredValue = `${enteredDate.value} ${month}`
+    return enteredValue
+}
+
+function submit(){ 
+    setDate();
  // used Fetch API to render the data
     fetch('https://api.covid19india.org/data.json')
 	.then(response => response.json())
-	.then(({ cases_time_series: cases }) => cases
-        .find(x => {
-           if(x.date === enteredValue + ' '){ //here x is the data.json
-               date = x.date
-           }
-			if(x.date === enteredValue + ' '){
-                document.getElementById('dailyconfirmed').textContent = x.dailyconfirmed;
-                document.getElementById('dailydeceased').textContent = x.dailydeceased;  
-                document.getElementById('dailyrecovered').textContent = x.dailyrecovered;
-                document.getElementById('totalconfirmed').textContent = x.totalconfirmed;      
-                document.getElementById('totaldeceased').textContent = x.totaldeceased;
-                document.getElementById('totalrecovered').textContent = x.totalrecovered;    
-            }
-		})
-    )
+	.then(({ cases_time_series: cases }) =>
+            cases.find(
+            x => {
+            if(x.date === enteredValue + ' '){ //here x is the data.json
+    //               date = x.date;
+                        document.getElementById('dailyconfirmed').textContent = x.dailyconfirmed;
+                        document.getElementById('dailydeceased').textContent = x.dailydeceased;  
+                        document.getElementById('dailyrecovered').textContent = x.dailyrecovered;
+                        document.getElementById('totalconfirmed').textContent = x.totalconfirmed;      
+                        document.getElementById('totaldeceased').textContent = x.totaldeceased;
+                        document.getElementById('totalrecovered').textContent = x.totalrecovered;    
+                }
+            })
+        )
 }
-
+// End of Main Code
+$(document).ready(() => {
+	$.getJSON('https://covid19.mathdro.id/api', (data) => {
+		$('#confirmed').html(data.confirmed.value);
+		$('#active').html(data.confirmed.value - data.recovered.value - data.deaths.value);
+		$('#recovered').html(data.recovered.value);
+		$('#deaths').html(data.deaths.value);
+		$('#recovery-rate').html('Recovery Percentage: '+Math.floor((data.recovered.value/data.confirmed.value)*100)+'% (approx*)')
+		$('#death-rate').html('Death Percentage: '+Math.floor((data.deaths.value/data.confirmed.value)*100)+'% (approx*)')
+    })
+    $.getJSON('https://covid19.mathdro.id/api/countries/india', (data) => {
+		$('#confirmedI').html(data.confirmed.value);
+		$('#activeI').html(data.confirmed.value - data.recovered.value - data.deaths.value);
+		$('#recoveredI').html(data.recovered.value);
+		$('#deathsI').html(data.deaths.value);
+		$('#recovery-rateI').html('Recovery Percentage: '+Math.floor((data.recovered.value/data.confirmed.value)*100)+'% (approx*)')
+		$('#death-rateI').html('Death Percentage: '+Math.floor((data.deaths.value/data.confirmed.value)*100)+'% (approx*)')
+	})
+})
